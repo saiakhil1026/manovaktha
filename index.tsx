8c75fa4b6182dea2ab91b7431d7071913ad09570
saiakhil1026 <akhilayanampudiakhil@gmail.com>
2025-10-29 03:17:43 +0530

    Fix merge conflicts in index.html and index.tsx

--- index.tsx
+++ index.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { LanguageProvider } from './contexts/LanguageContext';
-<<<<<<< HEAD
 import { AuthProvider } from './contexts/AuthContext';
-=======
->>>>>>> 39ceae5246b9efa5d915fc623f5e55a25c810605
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -15,16 +12,10 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <AuthProvider>
       <LanguageProvider>
         <App />
       </LanguageProvider>
     </AuthProvider>
-=======
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
->>>>>>> 39ceae5246b9efa5d915fc623f5e55a25c810605
   </React.StrictMode>
 );
